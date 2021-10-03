62d7722862a4b426b23347b629ae6f02f8939844
Parag Katoch <paragkatoch5@gmail.com>
2021-10-03 13:07:12 +0530

    replaced images with original links

--- src/Components/AppIcon.js
+++ src/Components/AppIcon.js
@@ -2,11 +2,14 @@ import React from "react";
 import "../styles/AppIcon.scss";
 
 export default function AppIcon() {
-  return (
-    <div className="app_icon_container">
-      <a href="/">
-        <img src="/assets/AppIcon.jpg" alt="Cartoon Network Logo" />
-      </a>
-    </div>
-  );
+	return (
+		<div className="app_icon_container">
+			<a href="/">
+				<img
+					src="https://assets.digicorus.corusdigitaldev.com/wp-content/uploads/sites/19/2018/05/29195345/CartoonNetwork_NavLogo_120x72.jpg"
+					alt="Cartoon Network Logo"
+				/>
+			</a>
+		</div>
+	);
 }
--- src/Containers/FeaturedCards.js
+++ src/Containers/FeaturedCards.js
@@ -6,49 +6,49 @@ const featuredData = [
 	{
 		title: "BEN 10",
 		info: "Weekdays 3:30pm ET/PT",
-		img: "/assets/featureImage1.jpg",
+		img: "https://assets.digicorus.corusdigitaldev.com/wp-content/uploads/sites/19/2018/06/04133345/CartoonNetwork_Ben10_462x386.jpg",
 	},
 	{
 		title: "Ok. K.O! Let's Be Heroes",
 		info: "Weekdays 3:30pm ET/PT",
-		img: "/assets/featureImage2.jpg",
+		img: "https://assets.digicorus.corusdigitaldev.com/wp-content/uploads/sites/19/2018/05/29200714/CartoonNetwork_OKKO_462x386.jpg",
 	},
 ];
 const restFeaturedData = [
 	{
 		title: "Steven Universe",
 		info: "Weekends 11:30am ET/PT",
-		img: "/assets/featureImage3.jpg",
+		img: "https://assets.digicorus.corusdigitaldev.com/wp-content/uploads/sites/19/2018/05/29200734/CartoonNetwork_StevenUniverse_462x386.jpg",
 	},
 	{
 		title: "We Bare Bears",
 		info: "Weekdays 6:00pm ET/PT",
-		img: "/assets/featureImage4.jpg",
+		img: "https://assets.digicorus.corusdigitaldev.com/wp-content/uploads/sites/19/2018/06/04142757/CartoonNetwork_WeBareBears_462x3861.jpg",
 	},
 	{
 		title: "The Powerpuff Girls",
 		info: "Every day at 7:00am ET/PT",
-		img: "/assets/featureImage5.jpg",
+		img: "https://assets.digicorus.corusdigitaldev.com/wp-content/uploads/sites/19/2018/05/29200805/CartoonNetwork_ThePowerPuffGirls_462x386.jpg",
 	},
 	{
 		title: "The Regular Show",
 		info: "Weekends 7:30pm ET/PT",
-		img: "/assets/featureImage6.jpg",
+		img: "https://assets.digicorus.corusdigitaldev.com/wp-content/uploads/sites/19/2018/05/29200826/CartoonNetwork_TheRegularShow_462x386.jpg",
 	},
 	{
 		title: "Adventure Time",
 		info: "Weekends NOON ET/PT",
-		img: "/assets/featureImage7.jpg",
+		img: "https://assets.digicorus.corusdigitaldev.com/wp-content/uploads/sites/19/2018/05/29200850/CartoonNetwork_AdventureTime_462x386.jpg",
 	},
 	{
 		title: "Apple and Onion",
 		info: "Weekends 1:30pm ET/PT",
-		img: "/assets/featureImage8.jpg",
+		img: "https://assets.digicorus.corusdigitaldev.com/wp-content/uploads/sites/19/2020/10/01130736/AppleAndOnion.jpg",
 	},
 	{
 		title: "The Amazing World of Gumball",
 		info: "Weekdays 7:00pm ET/PT",
-		img: "/assets/featureImage9.jpg",
+		img: "https://assets.digicorus.corusdigitaldev.com/wp-content/uploads/sites/19/2020/10/01130745/AWOG.jpg",
 	},
 ];
 
